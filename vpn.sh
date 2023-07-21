96442d38f42b856e08045ca262ffd7ec5e648eb7
Jan-Karel <info@jan-karel.nl>
2023-07-21 15:36:55 +0200

    plus mimikatz

---
+++ vpn.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source meuk/globalmeuk.sh
+
+systeem=${1:-}
+
+
+if [ -z "$systeem" ]; then
+  echo "[.] Please provide a system to RDP into"
+  exit;
+fi
+
+xfreerdp /u:jan-karel /p:C0mpl3x.teit /p:C0mpl3x.teit /smart-sizing:1920x1080 +clipboard /v:${systeem}
